6adfc5929e5fec54ca7618d041c2573cc8a8d3ac
Axel Garcia <axel@Axels-Device.local>
2019-04-08 16:38:09 -0700

    Updates

--- precache-manifest.bc0c18a6188c2fe34ffaaf1f3c863561.js
+++ precache-manifest.bc0c18a6188c2fe34ffaaf1f3c863561.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a4ce8596fdd18e378ca2",
+    "revision": "602fba2536ddfeab8623",
     "url": "/postercreatorpancan/static/css/main.b4911899.chunk.css"
   },
   {
-    "revision": "a4ce8596fdd18e378ca2",
-    "url": "/postercreatorpancan/static/js/main.a4ce8596.chunk.js"
+    "revision": "602fba2536ddfeab8623",
+    "url": "/postercreatorpancan/static/js/main.602fba25.chunk.js"
   },
   {
     "revision": "21d57814104ff7feb958",
@@ -20,12 +20,16 @@ self.__precacheManifest = [
     "url": "/postercreatorpancan/static/js/runtime~main.ccb320ea.js"
   },
   {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/postercreatorpancan/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/postercreatorpancan/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/postercreatorpancan/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/postercreatorpancan/static/media/flags.9c74e172.png"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
@@ -34,10 +38,6 @@ self.__precacheManifest = [
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/postercreatorpancan/static/media/outline-icons.701ae6ab.eot"
-  },
-  {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/postercreatorpancan/static/media/flags.9c74e172.png"
   },
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
@@ -56,16 +56,16 @@ self.__precacheManifest = [
     "url": "/postercreatorpancan/static/media/brand-icons.a046592b.woff"
   },
   {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/postercreatorpancan/static/media/icons.8e3c7f55.eot"
+  },
+  {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/postercreatorpancan/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/postercreatorpancan/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/postercreatorpancan/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/postercreatorpancan/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "adb74cb1ec15ec57d1314dd313f1de0f",
+    "revision": "7154f8cd2d3df717cdf1a147abdc370c",
     "url": "/postercreatorpancan/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/postercreatorpancan/precache-manifest.9a622cb062e9981f5fb895f2ed8dcc48.js"
+  "/postercreatorpancan/precache-manifest.bc0c18a6188c2fe34ffaaf1f3c863561.js"
 );
 
 workbox.clientsClaim();
